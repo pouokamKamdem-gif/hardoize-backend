c4cdd4e94f1a737f8368c6de94bd4f35df118244
pouokamKamdem-gif <pouokamjoyce@gmail.com>
2026-07-10 10:38:02 +0200

    Update HistoriqueVenteRepository.java

--- src/main/java/com/digneequipe/hardoize/repositories/HistoriqueVenteRepository.java
+++ src/main/java/com/digneequipe/hardoize/repositories/HistoriqueVenteRepository.java
@@ -3,6 +3,12 @@ package com.digneequipe.hardoize.repositories;
 import com.digneequipe.hardoize.models.HistoriqueVente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface HistoriqueVenteRepository
         extends JpaRepository<HistoriqueVente, Long> {
+    Optional<HistoriqueVente> findByUuid(String uuid);
+
+    List<HistoriqueVente> findByGroupeId(Long groupeId);
 }
